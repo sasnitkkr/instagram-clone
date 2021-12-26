fc9229dfd47559c7f2d30262efad5f92cc0799a6
Sahil Shende <ssahilshende@gmail.com>
2021-12-26 18:28:07 +0530

    5 - add user to database

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
 router.get("/signup", (req, res) => {
   const { name, email, password } = req.body;
-  console.log(name);
-  console.log(email);
-  console.log(password);
+
   if (!name || !email || !password) {
     return res.status(422).json({ error: "Enter all valid fields" });
   }
-  res.json({ message: "Successfully Submitted" });
+  User.findOne({ email: email }, function (err, foundUser) {
+    if (err) {
+      console.log("Error finding user with given mail.");
+      console.log(err);
+    } else {
+      if (foundUser) {
+        res.status(422).json({ err: "User already exists with given email." });
+      } else {
+        const newUser = new User({
+          name: name,
+          email: email,
+          password: password,
+        });
+        newUser.save();
+        res.json({ message: "Successfully saved user" });
+      }
+    }
+  });
 });
 
 module.exports = router;
