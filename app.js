81fc6e6ef318335a05b6378a2a0d2b1bfbca8b05
Sahil Shende <ssahilshende@gmail.com>
2021-12-26 11:04:07 +0530

    set basic middleware

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;  
+const PORT = 3000;
+
+function customMiddleware(req, res, next) {
+  console.log("Middleware Executed");
+  next();
+}
 
 app.get("/", (req, res) => {
+  console.log("Inside Home route");
   res.send("Hello Insta");
+});
+
+app.get("/about", customMiddleware, (req, res) => {
+  console.log("Inside about route");
+  res.send("Hello About");
 });
 
 app.listen(PORT, () => {
